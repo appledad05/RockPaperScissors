525802b3558adacc7e4aaf873b7c68d75d5d4bc3
Mark J Perryman <appledad05@gmail.com>
2020-11-24 13:29:08 -0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Mark Perryman on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
